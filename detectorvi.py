aabe24ea0e6a9408db14b138906fe36b163f8416
Ajo George <ajogeorge2003@gmail.com>
2024-04-26 22:02:42 +0530

    website

--- detectorvi.py
+++ detectorvi.py
@@ -17,7 +17,7 @@ firebase_admin.initialize_app(cred, {
 
 reader = easyocr.Reader(['en'], gpu=True)
 
-cap = cv2.VideoCapture("../videos/parking_data4.mov")
+cap = cv2.VideoCapture("../videos/parking_data7.mov")
 
 model = YOLO("../Yolo-Weights/best_l.pt")
 
