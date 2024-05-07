a639c821236a02786dd2bc518b9d1729e6c800d8
Ajo George <ajogeorge2003@gmail.com>
2024-05-07 23:19:42 +0530

    some modifications

--- web/active_log/active_log.js
+++ web/active_log/active_log.js
@@ -95,7 +95,13 @@ async function fetchData(plateNumber, in_timestamp) {
       alert("No data found for plate number: " + plateNumber);
     }
   } else {
-    console.log("No such document for plate number:", plateNumber);
+    console.log("No such document for plate number:"+ plateNumber);
+    // alert("No Registered number as: " + plateNumber);
+    activePlatesEl.innerHTML += `
+      <li style="background-color: red; color: black;">Plate Number: ${plateNumber}</li>
+      <li style="background-color: red; color: black;">In: ${new Date(in_timestamp).toLocaleString()}</li>
+      <hr>
+    `;
   }
 }
 
